96543a98e384483cf9655458bee623c9de1db0ec
jangbohyeon <bvcxz0525@gmail.com>
2021-06-09 23:18:20 +0900

    update

---
+++ src/myconfig.py
@@ -0,0 +1,18 @@
+import os
+from configparser import SafeConfigParser
+
+def get_config(filepath):
+    """Get config parser.
+
+    :param filepath: a config filepath to read.
+    :type filepath: str
+    :return: a parser (Use it like a "dictionary")
+    :rtype: configparser.SafeConfigParser
+    """
+    parser = SafeConfigParser(os.environ)
+    ret = parser.read(filepath)
+    if not ret:
+        print('get_config(): Failed to parser.read()')
+        return False
+    return parser
+
