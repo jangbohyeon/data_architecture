7966bff0132bb62f718242fbd7f4b9b4827e9992
jangbohyeon <49804605+jangbohyeon@users.noreply.github.com>
2021-06-12 20:57:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import sys
+import sys
+import datetime
+import os
 import pdb
 
 def test_logger():
     """Test logger.
 
-    :return: test result.
-    :rtype: bool
+    :return: logger or False
+    :rtype: logging.Logger or bool
     """
     from src import mylogger
     try:
@@ -14,7 +17,7 @@ def test_logger():
     except Exception as e:
         print(e)
         return False
-    return True
+    return m
 
 def test_config():
     """Test config.
@@ -24,7 +27,8 @@ def test_config():
     """
     from src import myconfig
     try:
-        m = myconfig.get_config('/root/data_architecture/share/test.config')
+        project_root_path = os.getenv("DATA_ARCHITECTURE")
+        m = myconfig.get_config('{}/share/test.config'.format(project_root_path))
         print('key1=', m['general'].get('key1'))
         print('key2=', m['general'].get('key2'))
         print('key3=', m['logger'].get('key3'))
@@ -33,28 +37,21 @@ def test_config():
         return False
     return True
 
-def test_login(db):
+def test_login(logger):
     """Test login process.
 
-    :param db: Database
-    :type db: database instance
+    :param logger: logger instance
+    :type logger: logging.Logger
     :return: test result
     :rtype: bool
     """
     from src import user
-    col_user = db['User']
-    doc_user = user.check_passwd(col_user, 'iam2', 'biggong')
-    if not doc_user:
-        return False
-    print("Login test for user = ", doc_user["user_id"])
-
-    session_key = user.generate_session(doc_user, col_user)
+    session_key = user.login('iam', '1234', logger)
     if not session_key:
         return False
-    print("generated session = {}".format(session_key))
 
     what_time_is_it = datetime.datetime.now()
-    doc_user_result = user.check_session(col_user,
+    doc_user_result = user.check_session( \
             session_key["session_id"],
             what_time_is_it.timestamp())
     if not doc_user_result:
@@ -68,22 +65,10 @@ if __name__ == '__main__':
         target_step = sys.argv[1].split(',')
     print('Test steps = ', target_step)
 
-    import pymongo
-    from pymongo import MongoClient
-    db_info = {
-        'ip': '127.0.0.1',
-        'port': 27017,
-        'db': 'tonic_db'
-    }
-    db_client = MongoClient(db_info['ip'], db_info['port'])
-    db = db_client[db_info['db']]
-    print('Success - DB connection')
-
-    if not target_step or 'logger' in target_step:
-        ret = test_logger()
-        if not ret:
-            raise Exception('Error when test_logger')
-        print('Success - test_logger')
+    logger = test_logger()
+    if not logger:
+        raise Exception('Error when test_logger')
+    print('Success - test_logger')
 
     if not target_step or 'config' in target_step:
         ret = test_config()
@@ -92,9 +77,7 @@ if __name__ == '__main__':
         print('Success - test_config')
 
     if not target_step or 'login' in target_step:
-        if not db:
-            raise Exception('Error when test_login (due to DB connection)')
-        ret = test_login(db)
+        ret = test_login(logger)
         if not ret:
             raise Exception('Error when test_login')
         print('Success - test_login')
