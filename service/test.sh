0aafca9010132782fe2af199efddfb04a80e19a3
jangbohyeon <49804605+jangbohyeon@users.noreply.github.com>
2021-06-12 16:29:53 +0900

    Update test.sh

--- service/test.sh
+++ service/test.sh
@@ -2,9 +2,9 @@ if [ $# -lt 1 ]; then
         echo 'give 1 argument'
 else
         if [ $1 = 'login' ]; then
-                curl -H "Content-type: application/json" -X POST -d '{"user_id":"iam","passwd":“1111"}' http://118.67.135.152:8080/login >> ret
+                curl -H "Content-type: application/json" -X POST -d '{"user_id":"iam","passwd":“1111"}' http://127.0.0.1:1234/login >> ret
         elif [ $1 = 'favorite' ]; then
-                curl -H "Content-type: application/json" -X POST -d '{"session_id":"xxxxxxxx"}' http://118.67.135.152:8080/favorite >> ret
+                curl -H "Content-type: application/json" -X POST -d '{"session_id":"xxxxxxxx"}' http://127.0.0.1:1234/favorite >> ret
         fi
 fi
 
